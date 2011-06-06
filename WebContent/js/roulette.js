0c420e8f83fa8ba2e33f3f2614833435ee688a45
robmcguinness <robert.mcguinness.iii@gmail.com>
2011-06-05 22:54:50 -0400

    (1) adding actions buttons (2) refactoring roulette.js
    Signed-off-by: robmcguinness <robert.mcguinness.iii@gmail.com>

--- WebContent/js/roulette.js
+++ WebContent/js/roulette.js
@@ -10,10 +10,13 @@ $(function() {
 	var h = $(document).height(); // full h of html
 	var rOuter = h / 2.2; // prevents wheel edge from hitting viewport edge
 	var rInner = rOuter / 2;
-	var strokeWidth = 3;
+	var strokeWidth = 1;
 	var sectionAngle = 360 / prizes.length;
 	var curvePoint = Math.PI / 180;
 	var color = ["#77A636", "#E0BC32", "#7EC7D0"];
+	var colorArrow = ["#800080", "#FFCEFF"]; // [fill, border]
+	var sections = {};
+	var random = [1000, 4000];
 
 	// go full screen
 	var paper = Raphael(0, 0, w, h);
@@ -28,8 +31,18 @@ $(function() {
 		drawCircle();
 		drawSections();
 
-	};
+		var arrow = drawArrow();
+		var el = document.elementFromPoint(center.x+rInner+10, center.y);
 
+		$("#spin").click(function() {
+			spin();
+		});
+
+		$("#stop").click(function() {
+			sections.stop();
+		});
+
+	};
 
 	/**
 	 * Finds the start and end point for arc on a circle with a given radius, start and end angle
@@ -58,32 +71,39 @@ $(function() {
 		var beginAngle = 0;
 		var endAngle = sectionAngle;
 
-		var section = paper.set();
+		sections = paper.set();
 
 		for(var i = 0; i < prizes.length; i++) {
 			var points = getPoints(center.x, center.y, rOuter, rInner, beginAngle, endAngle);
 
-			var edge1 = drawLine(points, true);
-			debugger;
+			var edge1 = drawSectionBorder(points, true);
+
 			var arc1 = drawArc(rOuter, beginAngle, endAngle, points);
 
-			var edge2 = drawLine(points, false);
+			var edge2 = drawSectionBorder(points, false);
 
-			section.push(paper.path(edge1 + arc1 + edge2 + " z").attr({ // z close path
+			var section = paper.path(edge1 + arc1 + edge2 + " z").attr({ // z close path
 				stroke: color[i % color.length],
 				"stroke-width" : strokeWidth,
 				fill: color[i % color.length]
+			});
+			section.node.id = 'section'+i;
 
-			}));
+			sections.push(section);
+
+			/*
+			sections.onAnimation(function () {
+				console.log("test");
+			});
+			*/
 
 			beginAngle = endAngle;
 			endAngle += sectionAngle;
 		}
 
-		section.animate({rotation:"300 " + center.x + " " + center.y}, 10000,'>');
 	};
 
-	var drawLine = function(points, isLeftBorder) {
+	var drawSectionBorder = function(points, isLeftBorder) {
 		var _edge;
 		if(isLeftBorder) {
 			_edge = 'M' + points.inner.x1+ ' ' + points.inner.y1 + "L" + points.outer.x1 + ' ' + points.outer.y1;
@@ -99,6 +119,30 @@ $(function() {
 			"stroke" : "#E1E1E1",
 			"stroke-width" : strokeWidth
 		});
+		circle.id = "circle";
+	};
+
+	var drawArrow = function() {
+		var arrow = paper.path("M"+(center.x+rOuter-10)+" "+center.y+"l30 -10l0 20z").attr({
+			stroke: colorArrow[1],
+			"stroke-width": 2,
+			fill: colorArrow[0]
+		});
+		arrow.id = "arrow";
+		return arrow;
+
+	};
+
+	var drawReferenceLine = function() {
+		var wRect = 20;
+		var topLeftX = center.x + rOuter - (wRect/2);
+		var topLeftY = center.y - (wRect/2);
+
+		var refLine = paper.path("M"+center.x + " " + center.y+"L"+(center.x+rOuter+10)+" " + center.y).attr({
+			"stroke" : "#0066CC",
+			"stroke-width" : strokeWidth
+		});
+
 	};
 
 	var drawArc = function(radius, startAngle, endAngle, points) {
@@ -120,6 +164,19 @@ $(function() {
 		paper.clear();
 	};
 
+	var spin = function() {
+		sections.stop().animate({rotation: randomFromTo() + " " + center.x + " " + center.y}, 10000,'>');
+	};
+
+	var randomFromTo = function() {
+		var number = Math.floor(Math.random() * (random[0] - random[1] + 1) + random[1]);
+		return number;
+	};
+
 	init();
+
+	return {
+		spin: spin
+	};
 
 });
